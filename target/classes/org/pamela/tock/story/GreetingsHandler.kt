d9b073ef51faea698538da3e6a944959b0fda78a
Pamela ANOU <panou@LYON.norsys.fr>
2018-10-29 16:55:52 +0100

    ajout des steps dans le fichier greetingsHandler.kt

--- target/classes/org/pamela/tock/story/GreetingsHandler.kt
+++ target/classes/org/pamela/tock/story/GreetingsHandler.kt
@@ -6,8 +6,31 @@ import fr.vsct.tock.bot.connector.messenger.buttonsTemplate
 import fr.vsct.tock.bot.connector.messenger.postbackButton
 import fr.vsct.tock.bot.connector.messenger.withMessenger
 import fr.vsct.tock.bot.definition.story
+import fr.vsct.tock.bot.definition.SimpleStoryStep
+import fr.vsct.tock.bot.definition.*
+
 import org.pamela.tock.story.logger
 
+
+enum class MyStep : SimpleStoryStep { greetings, prenomIntent, nomIntent, codeSecretFirstIntent, codeSecretSecondIntent, codeSecretThirdIntent }
+
+val story = storyWithSteps<MyStep>("stories") {
+    if(step == welcome) {
+        // ...
+    } else if(step == prenomIntent) {
+        // ...
+    } else if(step == nomIntent) {
+        // ...
+    } else if(step == codeSecretFirstIntent) {
+        // ...
+    } else if(step == codeSecretSecondIntent) {
+        // ...
+    } else if(step == codeSecretThirdIntent) {
+        // ...
+    }else {
+        //default case
+    }
+}
 /**
  * The greetings handler.
  */
@@ -23,8 +46,7 @@ val welcome = story("greetings") {
     withGoogleAssistant {
        gaMessage(
             "Pour une connexion sécurisé, pourriez vous me donnez votre prénom s'il vous plait?"
-        )
-    }
-
+        ) }
     end()
 }
+		 
